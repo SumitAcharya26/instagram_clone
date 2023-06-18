62a3b15101e53abf05f7dd7569fe3e1be8b15e23
Sumit Acharya <sumitacharya256@gmail.com>
2023-06-18 22:48:09 +0530

    common changes

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home:  Scaffold(appBar: AppBar(title: Text('Sumit'),),),
+      title: 'Instagram Clone',
+      theme: ThemeData.dark(),
+      home:  Scaffold(body: AppBar(title: Text('Sumit'),),),
     );
   }
 }
