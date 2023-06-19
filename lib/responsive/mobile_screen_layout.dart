fd7c8a761abf8c96c67f2a94d7ee6d57129ab79e
Sumit Acharya <sumitacharya256@gmail.com>
2023-06-19 10:28:55 +0530

    link app to firebase for android

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -5,6 +5,6 @@ class MobileScreenLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(body: Center(child: Text('This is mobile')));
   }
 }
--- lib/responsive/web_screen_layout.dart
+++ lib/responsive/web_screen_layout.dart
@@ -5,6 +5,8 @@ class WebScreenLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(child: Text('This is web')),
+    );
   }
 }
